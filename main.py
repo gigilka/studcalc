3d0af82ba94facab377f3922d80565efbead6124
Gigilka <kuchin-02@mail.ru>
2023-05-03 19:12:39 +0300

    icons add and wind fix

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import customtkinter
 
 # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_appearance_mode("dark")
+customtkinter.set_appearance_mode("light")
 # Themes: "blue" (standard), "green", "dark-blue"
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_default_color_theme("dark-blue")
 
 app = customtkinter.CTk()
 app.geometry("1000x700")
 app.title("Student Calculator")
-
+app.wm_iconbitmap(r"icons/icomain.ico")
 
 def button_event():
     print("button pressed")
     window = customtkinter.CTk()
+    window.title("test")
     window.geometry("500x500")
+    window.wm_iconbitmap(r"icons/icomain.ico")
+
     window.mainloop()
 
 
